27ba62a009336cace4c17cd66a7755ee01b321e6
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-06 15:44:35 +0100

    Do it quicker :)

--- src/reader.cpp
+++ src/reader.cpp
@@ -1,47 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cstring>
+
+#include <boost/algorithm/string.hpp>
 
 #include "reader.hpp"
 
-void trim(std::string& str){
-    std::stringstream trimmer;
-    trimmer << str;
-    str.clear();
-    trimmer >> str;
-}
+namespace {
+
+typedef std::string::const_iterator string_iter;
+typedef boost::iterator_range<string_iter> string_view;
 
 void remove_quotes(std::string& str){
-    if(str.size() < 2){
-        return;
-    }
-
-    if(str[0] == '"' && str[str.length() - 1] == '"'){
+    if(str.size() < 2 && str[0] == '"' && str[str.length() - 1] == '"'){
         str = str.substr(1, str.length() - 2);
     }
 }
 
-void split(const std::string& line, char delim, std::vector<std::string> &elems) {
-    std::stringstream ss(line);
-    std::string item;
+std::string get_item(std::vector<string_view>& contents, int index){
+    auto& item = contents[index];
+    
+    std::string value(item.begin(), item.end());
 
-    while(std::getline(ss, item, delim)) {
-        trim(item);
-        remove_quotes(item);
-        elems.push_back(item);
-    }
+    boost::trim(value);
+    remove_quotes(value);
+
+    return value;
 }
 
-std::vector<std::string> parse_gooda_hotspot_line(const std::string& line){
+std::vector<string_view> parse_gooda_hotspot_line(const std::string& line){
     //Keep only the interesting part
     std::string contents_line = line.substr(2, line.size() - 5);
 
-    std::vector<std::string> contents;
+    std::vector<string_view> contents;
 
-    split(contents_line, ',', contents);
+    boost::split(contents, contents_line, [](char c){return c == ',';});
 
     return contents;
 }
+
+} //End of anonymous namespace
 
 bool converter::read_spreadsheets(const std::string& directory, converter::Data& data){
     std::cout << "Import spreadsheets from " << directory << std::endl;
@@ -85,20 +84,18 @@ bool converter::read_spreadsheets(const std::string& directory, converter::Data&
     //The first hotspot line
     std::getline(hotspot_file, line);
     
-    while (line.substr(0, 3) != "[,\"") {
-        if(!line.empty()){
-            auto contents = parse_gooda_hotspot_line(line);
+    while (!line.empty()) {
+        auto contents = parse_gooda_hotspot_line(line);
 
-            converter::Function function;
-            function.name = contents[2];
-            function.file = "unknown";
-            function.total_count = 0;
-            function.entry_count = 0;
+        converter::Function function;
+        function.name = get_item(contents, 2);
+        function.file = "unknown";
+        function.total_count = 0;
+        function.entry_count = 0;
 
-            data.add_file_name(function.file);
+        data.add_file_name(function.file);
 
-            data.functions.push_back(function);
-        }
+        data.functions.push_back(function);
 
         std::getline(hotspot_file, line);
     }
