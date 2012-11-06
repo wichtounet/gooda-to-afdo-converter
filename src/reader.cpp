82fcd3c56477a3aef6fe64ff72a6a5280d90ec3e
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-06 17:19:31 +0100

    Start reading the asm files Cleanup

--- src/reader.cpp
+++ src/reader.cpp
@@ -7,12 +7,20 @@
 #include <boost/lexical_cast.hpp>
 
 #include "reader.hpp"
+#include "utils.hpp"
 
-#define FUNCTION_NAME 2
-#define UNHALTED_CORE_CYCLES 7
-#define INSTRUCTION_RETIRED 9
+//Hotspot indices
+#define HS_FUNCTION_NAME 2
+#define HS_UNHALTED_CORE_CYCLES 7
+#define HS_INSTRUCTION_RETIRED 9
 
 #define HOTSPOT_CSV "/function_hotspots.csv"
+
+#define ASM_FOLDER "/asm/"
+#define ASM_CSV "_asm.csv"
+
+#define SRC_FOLDER "/src/"
+#define SRC_CSV "_src.csv"
 
 namespace {
 
@@ -25,7 +33,7 @@ void remove_quotes(std::string& str){
     }
 }
 
-std::vector<string_view> parse_gooda_hotspot_line(const std::string& line){
+std::vector<string_view> parse_gooda_line(const std::string& line){
     //Keep only the interesting part
     std::string contents_line = line.substr(2, line.size() - 5);
 
@@ -57,6 +65,34 @@ gcov_type get_counter(std::vector<string_view>& contents, int index){
     return boost::lexical_cast<gcov_type>(value);
 }
 
+void skip_headers(std::ifstream& file){
+    std::string line;
+
+    //Introduction of the array
+    std::getline(file, line);
+
+    //Headers
+    std::getline(file, line);
+    
+    //Events
+    std::getline(file, line);
+    
+    //MSR Programming
+    std::getline(file, line);
+    
+    //Period
+    std::getline(file, line);
+    
+    //Multiplex
+    std::getline(file, line);
+    
+    //Penalty
+    std::getline(file, line);
+    
+    //Cycles
+    std::getline(file, line);
+}
+
 bool read_hotspot(const std::string& directory, converter::Data& data){
     std::string hotspot_file_name = directory + HOTSPOT_CSV;
 
@@ -68,42 +104,20 @@ bool read_hotspot(const std::string& directory, converter::Data& data){
         return false;
     }
 
+    skip_headers(hotspot_file);
+
     std::string line;
-
-    //Introduction of the array
-    std::getline(hotspot_file, line);
-
-    //Headers
-    std::getline(hotspot_file, line);
-    
-    //Events
-    std::getline(hotspot_file, line);
-    
-    //MSR Programming
-    std::getline(hotspot_file, line);
-    
-    //Period
-    std::getline(hotspot_file, line);
-    
-    //Multiplex
-    std::getline(hotspot_file, line);
-    
-    //Penalty
-    std::getline(hotspot_file, line);
-    
-    //Cycles
-    std::getline(hotspot_file, line);
 
     //The first hotspot line
     std::getline(hotspot_file, line);
     
-    while (!line.empty()) {
-        auto contents = parse_gooda_hotspot_line(line);
+    while(!line.empty()){
+        auto contents = parse_gooda_line(line);
 
         converter::Function function;
-        function.name = get_string(contents, FUNCTION_NAME);
+        function.name = get_string(contents, HS_FUNCTION_NAME);
         function.file = "unknown";
-        function.total_count = get_counter(contents, UNHALTED_CORE_CYCLES);
+        function.total_count = get_counter(contents, HS_UNHALTED_CORE_CYCLES);
         function.entry_count = 0;
 
         data.add_file_name(function.file);
@@ -126,6 +140,35 @@ bool converter::read_spreadsheets(const std::string& directory, converter::Data&
     //Import the hotspot functions
     if(!read_hotspot(directory, data)){
         return false;
+    }
+
+    for(std::size_t i = 0; i < data.functions.size(); ++i){
+        std::string asm_file_name = directory + ASM_FOLDER + std::to_string(i) + ASM_CSV;
+
+        if(converter::exists(asm_file_name)){
+            std::ifstream asm_file;
+            asm_file.open (asm_file_name, std::ios::in);
+
+            if(!asm_file.is_open()){
+                std::cout << "Unable to open \"" << asm_file_name << "\"" << std::endl;
+                return false;
+            }
+
+            skip_headers(asm_file);
+            
+            std::string line;
+
+            //The first hotspot line
+            std::getline(asm_file, line);
+
+            while(line.size() > 3){
+                auto contents = parse_gooda_line(line);
+
+                std::getline(asm_file, line);
+            }
+
+            std::cout << data.functions[i].name << ":" << data.functions[i].total_count << std::endl;
+        }
     }
 
     return true;
