80b84955343f3a6570933b07ece7af170978bd06
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-11-16 18:21:56 -0800

    Prepare a profiling option

--- src/main.cpp
+++ src/main.cpp
@@ -52,29 +52,66 @@ int main(int argc, char **argv){
             ("help,h", "Display this help message")
             ("output,o", po::value<std::string>()->default_value("fbdata.afdo"), "The name of the generated AFDO file")
             ("dump", "Dump the AFDO on standard output")
-            ("input-file", po::value<std::string>()->required(), "Directory containing the spreadsheets");
+            ("profile,p", "Profile the given application")
+            ("input-file", po::value<std::vector<std::string>>()->required(), "Directory containing the spreadsheets");
 
         po::positional_options_description p;
         p.add("input-file", -1);
 
         po::variables_map vm;
-        po::store(po::command_line_parser(argc, argv).options(description).positional(p).run(), vm);
+        
+        auto parsed = po::command_line_parser(argc, argv).options(description).positional(p).allow_unregistered().run();
+        po::store(parsed, vm);
 
         if(vm.count("help")){
             std::cout << description;
             return 0;
         }
+
+        //Profiling mode
+        if(vm.count("profile")){
+            auto further_options = po::collect_unrecognized(parsed.options, po::include_positional);
+
+            //TODO
+
+            return 0;
+        }
         
-        //Must be done after the test for help to handle required arguments
+        //Must be done after the test for help/profile to handle required arguments
         po::notify(vm);
+       
+        //No further options are allowed if not in profile mode
+        auto further_options = po::collect_unrecognized(parsed.options, po::exclude_positional);
+        if(!further_options.empty()){
+            std::cerr << "Error: Unrecognized options " << further_options[0];
 
-        std::string directory = vm["input-file"].as<std::string>();
+            for(std::size_t i = 1; i < further_options.size(); ++i){
+                std::cerr << ", " << further_options[i];
+            }
 
+            std::cerr << std::endl;
+
+            return 1;
+        }
+
+        auto input_files = vm["input-file"].as<std::vector<std::string>>();
+        
+        //Verify that only one directory is provided
+        if(input_files.size() > 1){
+            std::cerr << "Error: Only one directory can be analyzed at a time" << std::endl;
+
+            return 1;
+        }
+
+        std::string directory = input_files[0];
+
+        //The file must exists
         if(!gooda::exists(directory)){
             std::cerr << "Error \"" << directory << "\" does not exists" << std::endl;
             return 1;
         }
 
+        //The file must be a directory
         if(!gooda::is_directory(directory)){
             std::cerr << "Error: \"" << directory << "\" is not a directory" << std::endl;
             return 1;
