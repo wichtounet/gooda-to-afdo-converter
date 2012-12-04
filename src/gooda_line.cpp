5488123e498204dc37fe327fc93188361b6d81ee
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-04 14:30:36 -0800

    Apply the Boost Software License

--- src/gooda_line.cpp
+++ src/gooda_line.cpp
@@ -1,3 +1,10 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2012.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
 #include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 
