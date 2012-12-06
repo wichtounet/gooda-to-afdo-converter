2c65c3054792d37373b6d45299c698264221f775
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-05 17:07:53 -0800

    Refactor

--- src/afdo_reader.cpp
+++ src/afdo_reader.cpp
@@ -8,6 +8,25 @@
 #include "afdo_reader.hpp"
 #include "gcov_file.hpp"
 
+namespace {
+
+void read_string_table(gooda::gcov_file& gcov_file, gooda::afdo_data& data){
+    //AFDO TAG
+    gcov_file.read_unsigned();
+
+    //The length of the section
+    data.length_file_section = gcov_file.read_unsigned();
+
+    auto files = gcov_file.read_unsigned();
+
+    for(gcov_unsigned_t i = 0; i < files; ++i){
+        auto file_name = gcov_file.read_string();
+        data.add_file_name(file_name);
+    }
+}
+
+} //end of anonymous
+
 void gooda::read_afdo(const std::string& afdo_file, gooda::afdo_data& data, boost::program_options::variables_map& vm){
     gooda::gcov_file gcov_file;
 
@@ -25,18 +44,5 @@ void gooda::read_afdo(const std::string& afdo_file, gooda::afdo_data& data, boos
     //Unused unsigned
     gcov_file.read_unsigned();
 
-    //File names section
-    
-    //AFDO TAG
-    gcov_file.read_unsigned();
-
-    //The length of the section
-    data.length_file_section = gcov_file.read_unsigned();
-
-    auto files = gcov_file.read_unsigned();
-
-    for(gcov_unsigned_t i = 0; i < files; ++i){
-        auto file_name = gcov_file.read_string();
-        data.add_file_name(file_name);
-    }
+    read_string_table(gcov_file, data);
 }
