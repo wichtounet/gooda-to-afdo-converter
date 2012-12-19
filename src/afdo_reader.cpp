80dee61769709585d373629db791cde19faea496
Baptiste Wicht <baptiste.wicht@gmail.com>
2012-12-19 10:55:15 -0800

    Fix bug with the reading: Arguments are evaluated from right to left...

--- src/afdo_reader.cpp
+++ src/afdo_reader.cpp
@@ -25,8 +25,6 @@ void read_string_table(gooda::gcov_file& gcov_file, gooda::afdo_data& data){
     for(gcov_unsigned_t i = 0; i < files; ++i){
         auto file_name = gcov_file.read_string();
         data.add_file_name(file_name);
-    
-        log::emit<log::Debug>() << "Found file name \"" << file_name << "\"" << log::endl;
     }
 }
 
@@ -51,13 +49,14 @@ void read_function_profile(gooda::gcov_file& gcov_file, gooda::afdo_data& data, 
             gooda::afdo_stack stack;
 
             auto size = gcov_file.read_unsigned();
+            std::cout << "size=" << size << std::endl;
             for(gcov_unsigned_t k = 0; k < size; ++k){
-                stack.stack.emplace_back(
-                            data.file_name(gcov_file.read_unsigned()), 
-                            data.file_name(gcov_file.read_unsigned()), 
-                            gcov_file.read_unsigned(),
-                            gcov_file.read_unsigned()
-                        );
+                auto func = data.file_name(gcov_file.read_unsigned());
+                auto file = data.file_name(gcov_file.read_unsigned());
+                auto line = gcov_file.read_unsigned();
+                auto disc = gcov_file.read_unsigned();
+
+                stack.stack.emplace_back(std::move(func), std::move(file), line, disc);
             }
 
             stack.count = gcov_file.read_counter();
