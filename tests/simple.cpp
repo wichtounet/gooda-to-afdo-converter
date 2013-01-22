1504ca1b20346137cad6ad30cd014af6629f9dd3
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-01-22 13:08:54 -0800

    Prepare a very simple sample to create unit test for the converter

---
+++ tests/simple.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#define SIZE 1000000
+
+long compute_sum(long* array){
+    long sum = 0;
+    for(int i = 0; i < 100; ++i){
+        for(int j = 0; j < SIZE; ++j){
+            sum += j * i * array[j];
+        }
+    }
+    return sum;
+}
+
+int main(){
+    long* array = new long[SIZE];
+
+    for(int i = 0; i < SIZE; ++i){
+        array[i] += i;
+    }
+
+    long sum = compute_sum(array);
+
+    std::cout << sum << std::endl;
+
+    delete[] array;
+
+    return 0;
+}
