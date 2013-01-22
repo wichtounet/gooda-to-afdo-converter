cd7a33b57fa9fbd661f89bb2d2364021b9309931
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-01-22 13:43:01 -0800

    Separate the arguments parsing to another class

---
+++ include/Options.hpp
@@ -0,0 +1,34 @@
+//=======================================================================
+// Copyright Baptiste Wicht 2012.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+//=======================================================================
+
+#ifndef GOODA_OPTIONS_HPP
+#define GOODA_OPTIONS_HPP
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+    
+namespace po = boost::program_options;
+
+namespace gooda {
+
+struct options {
+    po::options_description description;
+    po::variables_map vm;
+    po::parsed_options parsed_options;
+
+    options() : description("converter [options] spreadsheets_directory"), parsed_options(&description){
+        //Nothing to init
+    }
+    
+    int parse(int argc, char **argv);
+    int notify();
+};
+
+} //end of namespace gooda
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,6 @@
 #include <iostream>
 #include <chrono>
 
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/program_options/variables_map.hpp>
-
 #include "utils.hpp"
 #include "gooda_reader.hpp"
 #include "converter.hpp"
@@ -20,8 +16,7 @@
 #include "afdo_reader.hpp"
 #include "logger.hpp"
 #include "diff.hpp"
-    
-namespace po = boost::program_options;
+#include "Options.hpp"
 
 namespace {
 
@@ -195,104 +190,33 @@ int profile_application(po::variables_map& vm, po::parsed_options& parsed_option
 } //end of anonymous namespace
 
 int main(int argc, char **argv){
-    po::options_description description("converter [options] spreadsheets_directory");
-
-    po::variables_map vm;
-    po::parsed_options parsed_options(&description);
+    gooda::options options;
     
-    try {
-        po::options_description input("Input actions");
-        input.add_options()
-            ("read-spreadsheets", "Read Gooda spreadsheets (default)")
-            ("read-afdo", "Read an existing AFDO profile")
-            ("profile,p", "Profile the given application.")
-            ("diff", "Diff between two sets of spreadsheets (prototype)")
-            ;
-        
-        po::options_description output("Output actions");
-        output.add_options()
-            ("dump", "Dump AFDO profile on standard output")
-            ("full-dump", "Dump complete AFDO profile on standard output")
-            ("afdo", "Generate an AFDO profile (default if --profile is not selected)")
-            ;
-        
-        po::options_description afdo("AFDO Options");
-        afdo.add_options()
-            ("lbr", "Performs precise profile with LBR")
-            ("nows", "Do not compute the working set")
-            ("cache-misses", "Fill cache misses information in the AFDO file")
-            ("discriminators", "Find the DWARF discriminators of instructions, need >=binutils.2.23.1")
-            ;
+    int code = options.parse(argc, argv);
 
-        po::options_description others("Other Options");
-        others.add_options()
-            ("help,h", "Display this help message")
-            
-            ("bench", "Use the special scripts for benchmarking")
-            ("debug", "Make the dump more verbose")
-
-            //Ideally filter would have a std::string with an implicit empty string
-            //There is a bug in Boost PO that prevent implicit value and positional options at the same time
-            ("filter,f", "Only consider the hottest process.")
-            ("process", po::value<std::string>(), "Filter the hotspot functions by process.")
-            ("output,o", po::value<std::string>()->default_value("fbdata.afdo"), "The name of the generated AFDO file")
-            ("log", po::value<int>()->default_value(0), "Define the logging verbosity (0: No logging, 1: warnings, 2:debug 3:trace)")
-            ("quiet", "Output as less as possible on the console")
-
-            ("gooda", po::value<std::string>(), "Path to the Gooda installation directory. By default, $GOODA_DIR or the current directory will be used")
-            ("addr2line", po::value<std::string>()->default_value("addr2line"), "Specify the addr2line executable to use")
-            ("input-file", po::value<std::vector<std::string>>(), "Input file(s)");
-
-        description.add(input).add(output).add(afdo).add(others);
-
-        po::positional_options_description p;
-        p.add("input-file", -1);
-
-        parsed_options = po::command_line_parser(argc, argv).options(description).positional(p).allow_unregistered().run();
-        po::store(parsed_options, vm);
-
-        if(vm.count("help")){
-            std::cout << description;
-            return 0;
-        }
-    } catch (std::exception& e ) {
-        log::emit<log::Error>() << e.what() << log::endl;
-        return 1;
+    if(code != 0){
+        return code;
     }
 
-    log::set_level(vm["log"].as<int>());
+    log::set_level(options.vm["log"].as<int>());
 
-    if(vm.count("lbr") && vm.count("cache-misses")){
+    if(options.vm.count("lbr") && options.vm.count("cache-misses")){
         log::emit<log::Error>() << "Gooda does not support cache misses in LBR mode" << log::endl;
         return 1;
     }
 
     //Profiling mode
-    if(vm.count("profile")){
-        return profile_application(vm, parsed_options);
+    if(options.vm.count("profile")){
+        return profile_application(options.vm, options.parsed_options);
     }
     
-    //No further options are allowed if not in profile mode
-    auto further_options = po::collect_unrecognized(parsed_options.options, po::exclude_positional);
-    if(!further_options.empty()){
-        std::cerr << "Error: Unrecognized options " << further_options[0];
+    code = options.notify();
 
-        for(std::size_t i = 1; i < further_options.size(); ++i){
-            std::cerr << ", " << further_options[i];
-        }
-
-        std::cerr << std::endl;
-
-        return 1;
+    if(code != 0){
+        return code;
     }
 
-    try {
-        //Must be done after the test for help/profile to handle required arguments
-        po::notify(vm);
-    } catch (std::exception& e ) {
-        log::emit<log::Error>() << e.what() << log::endl;
-        return 1;
-    }
+    auto& vm = options.vm;
 
     if(!vm.count("input-file")){
         log::emit<log::Error>() << "No file provided" << log::endl;
