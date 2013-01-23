cfbd6780482bb95797d05293cab60b9433dd3373
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-01-22 16:00:25 -0800

    Implement test case for LBR

--- tests/UnitTest.cpp
+++ tests/UnitTest.cpp
@@ -34,7 +34,7 @@ inline void parse_options(gooda::options& options, std::string param1, std::stri
 
 BOOST_AUTO_TEST_SUITE(MainSuite)
 
-void check_contains_stack(gooda::afdo_function& function, unsigned int line, unsigned int discr, std::size_t count){
+void check_contains_stack(const gooda::afdo_function& function, unsigned int line, unsigned int discr, std::size_t count){
     bool found = false;
 
     for(auto& stack : function.stacks){
@@ -48,15 +48,17 @@ void check_contains_stack(gooda::afdo_function& function, unsigned int line, uns
                 BOOST_CHECK_EQUAL(pos.file, function.file);
                 BOOST_CHECK_EQUAL(pos.func, function.name);
 
+                BOOST_CHECK(stack.num_inst > 0);
+
                 found = true;
             }
         }
     }
 
-    BOOST_CHECK (found);
+    BOOST_CHECK(found);
 }
 
-void check_contains_inline_stack(gooda::afdo_function& function, unsigned int line1, unsigned int line2, std::string file, std::string inlined, std::size_t count){
+void check_contains_inline_stack(const gooda::afdo_function& function, unsigned int line1, unsigned int line2, std::string file, std::string inlined, std::size_t count){
     bool found = false;
 
     for(auto& stack : function.stacks){
@@ -64,19 +66,22 @@ void check_contains_inline_stack(gooda::afdo_function& function, unsigned int li
             auto& pos1 = stack.stack.front();
             auto& pos2 = stack.stack.back();
 
-            if(pos1.line == line1 && pos2.line == line2 && pos1.discriminator == 0 && pos2.discriminator == 0 && pos2.file == file && pos2.func == inlined){
+            if(pos1.line == line1 && pos2.line == line2 && pos1.discriminator == 0 && pos2.discriminator == 0 && pos2.file == file){
                 BOOST_CHECK(!found);
 
+                BOOST_CHECK_EQUAL(pos2.func, inlined);
                 BOOST_CHECK_EQUAL(stack.count, count);
                 BOOST_CHECK_EQUAL(pos1.file, function.file);
                 BOOST_CHECK_EQUAL(pos1.func, function.name);
+
+                BOOST_CHECK(stack.num_inst > 0);
 
                 found = true;
             }
         }
     }
 
-    BOOST_CHECK (found);
+    BOOST_CHECK(found);
 }
 
 BOOST_AUTO_TEST_CASE( simple_ucc ){
@@ -113,6 +118,47 @@ BOOST_AUTO_TEST_CASE( simple_ucc ){
 
     //Basic Block 11 (contains function inlined from standard library)
     check_contains_inline_stack(function, 24, 111, "ostream", "_ZNSolsEPFRSoS_E", 0);
+}
+
+BOOST_AUTO_TEST_CASE( simple_lbr ){
+    gooda::options options;
+    parse_options(options, "--lbr", "tests/cases/simple/");
+
+    //Read the Gooda Spreadsheets
+    auto report = gooda::read_spreadsheets("tests/cases/simple/lbr/spreadsheets");
+
+    gooda::afdo_data data;
+
+    //Convert the Gooda report to AFDO
+    gooda::convert_to_afdo(report, data, options.vm);
+
+    BOOST_CHECK_EQUAL (data.functions.size(), 1);
+
+    auto& function = data.functions.front();
+
+    //Verify function properties
+    BOOST_CHECK_EQUAL(function.name, "main");
+    BOOST_CHECK_EQUAL(function.file, "simple.cpp");
+    BOOST_CHECK_EQUAL(function.total_count, 939360);
+    BOOST_CHECK_EQUAL(function.entry_count, 0);
+
+    //Basic Block 3
+    check_contains_stack(function, 18, 2, 390);
+    check_contains_stack(function, 19, 2, 390);
+    
+    //Basic Block 4 (Verify that the lines with different discriminators are different)
+    check_contains_stack(function, 18, 0, 0);
+
+    //Basic Block 7 (No discriminator here, should be the sum)
+    check_contains_stack(function, 16, 0, 93780);
+
+    //Basic Block 8 (contains inlined functions)
+    check_contains_inline_stack(function, 22, 8, "simple.cpp", "compute_sum", 93780);
+    check_contains_inline_stack(function, 22, 9, "simple.cpp", "compute_sum", 93780);
+
+    //Basic Block 11 (contains function inlined from standard library)
+    check_contains_inline_stack(function, 24, 111, "ostream", "_ZNSolsEPFRSoS_E", 0);
+    check_contains_inline_stack(function, 24, 165, "ostream", "_ZNSolsEl", 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
